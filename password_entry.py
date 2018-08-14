e4c3cffa08f6cc3d8b1aaf7fe1a031c8bac43984
Mystyking <christopher.reppel@my.jcu.edu.au>
2018-08-14 15:23:56 +1000

    "List files in folder."

--- password_entry.py
+++ password_entry.py
@@ -2,15 +2,6 @@
 
 MINIMUM_LENGTH = 5
 
-
-def enter():
-    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-    while len(password) < MINIMUM_LENGTH:
-        password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
-
-    print('*' * len(password))
-
-
 def main():
     password = get_password(MINIMUM_LENGTH)
     print_asterisks(password)
@@ -19,7 +10,7 @@ def main():
 def get_password(minimum_length):
     password = input("Enter password of at least {} characters: ".format(minimum_length))
     while len(password) < minimum_length:
-        print("Password too short")
+        print("Password is too short")
         password = input("Enter password of at least {} characters: ".format(minimum_length))
     return password
 
